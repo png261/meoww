21b07b1ca8f5dda9ef325e217230df6044f5f508
png261 <nhphuong.code@gmail.com>
2023-06-18 23:04:17 +0700

    feat: init

---
+++ meoww/cat.py
@@ -0,0 +1,42 @@
+import pygame
+from .game import Game
+
+
+class Cat:
+    def __init__(self, x, y, width, height):
+        self.x = x;
+        self.y = y;
+        self.vx = x;
+        self.vy = y;
+        self.width = width
+        self.height = height
+        self.image = pygame.image.load("assets/images/cat.png")
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.is_flipped_x = False
+
+    def update(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        self.vx = (mouse_x - self.x) / 100
+        self.vy = (mouse_y - self.y) / 100
+
+        if not self.is_flipped_x  and self.vx > 0:
+            self.is_flipped_x = True
+            self.flip()
+
+        if self.is_flipped_x and self.vx < 0:
+            self.is_flipped_x = False
+            self.flip()
+
+        self.x += self.vx
+        self.y += self.vy
+
+        self.x = max(min(self.x, Game().window.width), 0)
+        self.y = max(min(self.y, Game().window.height), 0)
+    
+    def flip(self):
+        self.image = pygame.transform.flip(self.image, True, False)
+
+    def draw(self):
+        from .game import Game
+        Game().window.screen.blit(self.image, (self.x,self.y))
+
---
+++ meoww/cursor.py
@@ -0,0 +1,23 @@
+import pygame
+from .game import Game
+
+
+class Cursor:
+    def __init__(self, width=32, height=32):
+        self.width = width
+        self.height = height
+        self.image = pygame.image.load("assets/images/cursor.png")
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.x = 0
+        self.y = 0
+        pygame.mouse.set_visible(False)
+
+    def update(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        self.x = mouse_x 
+        self.y = mouse_y 
+
+    def draw(self):
+        Game().window.screen.blit(self.image, (self.x,self.y))
+
+
